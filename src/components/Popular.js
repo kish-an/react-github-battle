56a2c9a3bba7845f19590823a87c7f9339402365
Kishan <kishan.o@hotmail.com>
2020-02-21 19:20:06 +0000

    Add api call and render data to UI

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import LanguagesNav from './LanguagesNav';
+import { fetchPopularRepos } from '../utils/api';
 
 class Popular extends Component {
     constructor(props) {
@@ -7,19 +8,47 @@ class Popular extends Component {
 
         this.state = {
             language: 'All',
+            repos: null,
+            error: null,
         }
 
         this.updateLangauge = this.updateLangauge.bind(this);
+        this.isLoading = this.isLoading.bind(this);
+    }
+
+    componentDidMount() {
+        this.updateLangauge(this.state.language);
     }
 
     updateLangauge(language) {
         this.setState({
-            language
+            language,
+            repos: null,
+            error: null
+        });
+
+        fetchPopularRepos(this.state.language)
+        .then(repos => {
+            this.setState({
+                repos,
+                error: null
+            });
+        })
+        .catch(err => {
+            console.warn('Error fetching repos: ', err);
+
+            this.setState({
+                error: 'There was an error fetching the repositories.'
+            });
         });
     }
 
+    isLoading() {
+        return this.state.repos === null && this.state.error === null;
+    }
+
     render() {
-        const { language } = this.state;
+        const { language, repos, error } = this.state;
 
         return (
             <React.Fragment>
@@ -27,6 +56,12 @@ class Popular extends Component {
                     selectedLang={language}
                     onUpdateLang={this.updateLangauge}
                 />
+
+                {this.isLoading() && <p>Loading...</p>}
+
+                {error && <p>{error}</p>}
+
+                {repos && <pre> {JSON.stringify(repos, null, 2)} </pre>}
             </React.Fragment>
         )
     }
