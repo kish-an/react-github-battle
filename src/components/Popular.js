c11a6a595ea0b1777a7f2c3bb38defbf235fd5ee
Kishan <kishan.o@hotmail.com>
2020-02-21 02:18:47 +0000

    Add state to navbar

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
 
 class Popular extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            language: 'All',
+        }
+
+        this.updateLangauge = this.updateLangauge.bind(this);
+    }
+
+    updateLangauge(language) {
+        this.setState({
+            language
+        });
+    }
+
     render() {
         const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
 
         return (
-            <ul className="flex-center">
-                {languages.map(language => (
-                    <li key={language}>
-                        <button className="btn-clear nav-link">
-                            {language}
-                        </button>
-                    </li>
-                ))}
-            </ul>
+            <nav>
+                <ul className="flex-center">
+                    {languages.map(language => (
+                        <li key={language}>
+                            <button
+                            className="btn-clear nav-link"
+                            style={language === this.state.language ? { color: 'rgb(187, 46, 31)' } : null}
+                            onClick={()=> this.updateLangauge(language)}
+                            >
+                                {language}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
         )
     }
 }
