b2902bf8f75de97fb31acf1b0afef383176395ad
Kishan <kishan.o@hotmail.com>
2020-02-22 20:54:25 +0000

    Add Battle & Instructions

---
+++ src/components/Battle.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Instructions from './Instructions'
+
+class Battle extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Instructions />
+            </React.Fragment>
+        )
+    }
+}
+
+export default Battle;
+
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Popular from './components/Popular';
+import Battle from './components/Battle';
 
 class App extends Component {
     render(props) {
         return (
             <div className="container">
-                <Popular />
+                <Battle />
             </div>
         )
     }
