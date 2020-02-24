2e53ead492686c24ef45a0d805f3244d1eb71ffd
Kishan <kishan.o@hotmail.com>
2020-02-24 00:37:50 +0000

    Add PlayerInput form to Battle

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -4,18 +4,18 @@ import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa';
 function Instructions() {
     return (
         <div className="instructions-container">
-            <h1 class="center-text header-lg">Instructions</h1>
+            <h1 className="center-text header-lg">Instructions</h1>
             <ol className="container-sm grid center-text battle-instructions">
                 <li>
-                    <h3 class="header-sm">Enter two Github users</h3>
+                    <h3 className="header-sm">Enter two Github users</h3>
                     <FaUserFriends className="bg-light" color="rgb(255,191,116)" size={200} />
                 </li>
                 <li>
-                    <h3 class="header-sm">Battle</h3>
+                    <h3 className="header-sm">Battle</h3>
                     <FaFighterJet className="bg-light" color="#727272" size={200} />
                 </li>
                 <li>
-                    <h3 class="header-sm">See the winner</h3>
+                    <h3 className="header-sm">See the winner</h3>
                     <FaTrophy className="bg-light" color="rgb(255,215,0)" size={200} />
                 </li>
             </ol>
