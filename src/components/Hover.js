4dca4f6e839b8106c13dcc18689d5f6f8b181a07
Kishan <kishan.o@hotmail.com>
2020-02-25 21:36:42 +0000

    Change Tooltip hover logic from HOC to Render Prop
    - The Render Prop pattern avoids the disadvantages of Higher Order Components such as naming collisions and inversion of control

---
+++ src/components/Hover.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Hover extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+             hovering: false
+        }
+
+        this.handleMouseOver = this.handleMouseOver.bind(this);
+        this.handleMouseOut = this.handleMouseOut.bind(this);
+    }
+
+    handleMouseOver() {
+        this.setState({ hovering: true });
+    }
+
+    handleMouseOut() {
+        this.setState({ hovering: false });
+    }
+
+    render() {
+        return (
+            <div
+                onMouseOver={this.handleMouseOver}
+                onMouseOut={this.handleMouseOut}
+            >
+                {this.props.children(this.state.hovering)}
+            </div>
+        )
+    }
+}
+
+Hover.propTypes = {
+    children: PropTypes.func.isRequired,
+}
+
+export default Hover;
+
