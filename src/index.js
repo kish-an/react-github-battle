8c3fd0328192550619ffe4b53fedc46b3ce0c699
Kishan <kishan.o@hotmail.com>
2020-02-27 00:58:08 +0000

    Implement Code Splitting/Lazy Loading

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Popular from './components/Repos/Popular';
-import Battle from './components/Battle/Battle';
-import Results from './components/Battle/Results';
 import { ThemeProvider } from './contexts/theme';
 import Nav from './components/Navbar/Nav';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Loading from './components/Loading';
+
+const Popular = React.lazy(() => import('./components/Repos/Popular'));
+const Battle = React.lazy(() => import('./components/Battle/Battle'));
+const Results = React.lazy(() => import('./components/Battle/Results'));
 
 class App extends Component {
     state = {
@@ -26,14 +28,16 @@ class App extends Component {
                         <div className="container">
                             <Nav />
 
-                            <Switch>
-                                <Route exact path="/" component={Popular} />
-                                <Route exact path="/battle" component={Battle} />
-                                <Route path='/battle/results' component={Results} />
-                                <Route
-                                    render={() => <h1 style={{ textAlign: 'center', color: '#c0392b'}}>404 🚨</h1>}
-                                />
-                            </Switch>
+                            <React.Suspense fallback={<Loading />}>
+                                <Switch>
+                                    <Route exact path="/" component={Popular} />
+                                    <Route exact path="/battle" component={Battle} />
+                                    <Route path='/battle/results' component={Results} />
+                                    <Route
+                                        render={() => <h1 style={{ textAlign: 'center', color: '#c0392b'}}>404 🚨</h1>}
+                                    />
+                                </Switch>
+                            </React.Suspense>
                         </div>
                     </div>
                 </ThemeProvider>
